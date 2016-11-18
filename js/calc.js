c1a8b0cbaa80f2244a39855b3d62b37188d0683b
Mario Villani <villani.mario90@gmail.com>
2016-11-18 17:05:24 +0100

    code optimization
    less functions

--- js/calc.js
+++ js/calc.js
@@ -3,31 +3,25 @@
      // Default values for selects and inputs
      $scope.frags = $scope.sulfur = $scope.hqm = $scope.slots = 1;
      $scope.time_taken = 0;
-     $scope.calculateTime = function() {
-         $scope.time_taken = 2 * $scope.wood;
-     }
      $scope.sulfurChange = function() {
          $scope.wood = (2.5 * $scope.sulfur) / $scope.slots;
-         $scope.calculateTime();
      };
      $scope.fragsChange = function() {
          $scope.wood = (5 * $scope.frags) / $scope.slots;
-         $scope.calculateTime();
      };
      $scope.hqmChange = function() {
          $scope.wood = (10 * $scope.hqm) / $scope.slots;
-         $scope.calculateTime();
-
      };
      $scope.charcoalChange = function() {
-         $scope.wood = $scope.charcoal * 0.75;
-         $scope.calculateTime();
+         $scope.wood = ($scope.charcoal * 0.75);
      }
      $scope.woodChange = function() {
          $scope.sulfur = ($scope.slots * $scope.wood) / 2.5;
          $scope.frags = ($scope.slots * $scope.wood) / 5;
          $scope.hqm = ($scope.slots * $scope.wood) / 10;
-         $scope.charcoal = ($scope.wood / 0.75);
-         $scope.calculateTime();
      };
+     $scope.$watch("wood", function() { //Every time that wood changes
+         $scope.charcoal = ($scope.wood / 0.75);
+         $scope.time_taken = 2 * $scope.wood;
+     });
  });
