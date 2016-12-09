abee2c3e791a5d5e693d022701a81231ecc6f67c
Mario Villani <villani.mario90@gmail.com>
2016-12-09 20:36:52 +0100

    Support for Large Furnaces

--- js/calc.js
+++ js/calc.js
@@ -25,3 +25,12 @@
          $scope.time_taken = 2 * $scope.wood;
      });
  });
+ app.filter('range', function() {
+     return function(input, min, max) {
+         min = parseInt(min); //Make string input int
+         max = parseInt(max);
+         for (var i = min; i < max; i++)
+             input.push(i);
+         return input;
+     };
+ });
